6b3f46299f481261c653e6c86235a8012090d864
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-04-11 10:34:16 -0300

    terminado

--- Program.cs
+++ Program.cs
@@ -22,9 +22,7 @@ do{
                 Console.Clear();
                 break;
             case 3:
-                if(boxeador1 != null && boxeador2 != null){
-                    Pelear(boxeador1, boxeador2);
-                }
+                if(boxeador1 != null && boxeador2 != null) Pelear(boxeador1, boxeador2);
                 else Console.WriteLine("No se cargaron ambos boxeadores.");
                 break;
             case 4:
@@ -46,5 +44,16 @@ static Boxeador IngresarBoxeador(){
     return boxeador;
 }
 static void Pelear(Boxeador bx1, Boxeador bx2){
+    double skill1 = bx1.ObtenerSkill();
+    double skill2 = bx2.ObtenerSkill();
 
+    if(skill1 > skill2) GanadorDiferencia(skill1, skill2, bx1.Nombre);
+    else if (skill2 > skill1) GanadorDiferencia(skill2, skill1, bx2.Nombre);
+    else Console.WriteLine("Empataron!!");   
+}
+static void GanadorDiferencia(double ganador, double perdedor, string nom){
+    double diferencia = ganador - perdedor;
+    if(diferencia >= 30) Console.WriteLine($"Ganó {nom} por KO");
+    else if(diferencia >= 10) Console.WriteLine($"Ganó {nom} por puntos en fallo unánime");
+    else Console.WriteLine($"Ganó {nom} por puntos en fallo dividido");
 }
